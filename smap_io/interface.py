3e87a3267e7ffd457a0f943d45b43aba42bc075a
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2019-01-24 11:41:29 +0100

    Update TS reader class to pass kwargs to base class

--- smap_io/interface.py
+++ smap_io/interface.py
@@ -210,6 +210,38 @@ class SPL3SMP_Ds(MultiTemporalImageBase):
 class SMAPTs(GriddedNcOrthoMultiTs):
 
     def __init__(self, ts_path, grid_path=None, **kwargs):
+        '''
+        Class for reading SMAP time series after reshuffling.
+
+        Parameters
+        ----------
+        ts_path : str
+            Directory where the netcdf time series files are stored
+        grid_path : str, optional (default: None)
+            Path to grid file, that is used to organize the location of time
+            series to read. If None is passed, grid.nc is searched for in the
+            ts_path.
+
+        Optional keyword arguments that are passed to the Gridded Base:
+        ------------------------------------------------------------------------
+            parameters : list, optional (default: None)
+                Specific variable names to read, if None are selected, all are read.
+            offsets : dict, optional (default:None)
+                Offsets (values) that are added to the parameters (keys)
+            scale_factors : dict, optional (default:None)
+                Offset (value) that the parameters (key) is multiplied with
+            ioclass_kws: dict, (optional)
+                Optional keyword arguments to pass to OrthoMultiTs class:
+                ----------------------------------------------------------------
+                    read_bulk : boolean, optional (default:False)
+                        if set to True the data of all locations is read into memory,
+                        and subsequent calls to read_ts read from the cache and
+                        not from disk this makes reading complete files faster
+                    read_dates : boolean, optional (default:False)
+                        if false dates will not be read automatically but only on
+                        specific request useable for bulk reading because currently
+                        the netCDF num2date routine is very slow for big datasets.
+        '''
 
         if grid_path is None:
             grid_path = os.path.join(ts_path, "grid.nc")
