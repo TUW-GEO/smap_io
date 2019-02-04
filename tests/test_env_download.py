2b53e78f0934b4316b6517e0be9050874b4c7c28
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2019-02-04 19:32:39 +0100

    Add print statment to check downloaded data

--- tests/test_env_download.py
+++ tests/test_env_download.py
@@ -14,7 +14,8 @@ import time
 
 class DownloadTest(unittest.TestCase):
 
-    @unittest.skipIf("TRAVIS" not in os.environ or os.environ["TRAVIS"] == "false", "This runs only on Travis CI")
+    @unittest.skipIf("TRAVIS" not in os.environ or os.environ["TRAVIS"] == "false",
+                     "This runs only on Travis CI")
     def test_full_download(self):
 
         dl_path = tempfile.mkdtemp()
@@ -28,3 +29,5 @@ class DownloadTest(unittest.TestCase):
         #assert(os.listdir(dl_path) == ['2018.12.01', '2018.12.02'])
         #assert(os.listdir(os.path.join(dl_path, '2018.12.01')) == ['SMAP_L3_SM_P_20181201_R16020_002.h5'])
         #assert(os.listdir(os.path.join(dl_path, '2018.12.02')) == ['SMAP_L3_SM_P_20181202_R16020_001.h5'])
+        print(os.listdir(dl_path))
+        print(os.listdir(os.path.join(dl_path, '2018.12.01')))
