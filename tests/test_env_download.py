0b04cce4598eb9ad28e0cf570decd3a47ae722ef
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2019-02-04 20:05:54 +0100

    Update download test

--- tests/test_env_download.py
+++ tests/test_env_download.py
@@ -5,7 +5,6 @@ Tests that use passed login information from travis CI to download a few files
 '''
 
 import os
-import glob
 import tempfile
 
 from smap_io.download import main
@@ -14,20 +13,18 @@ import time
 
 class DownloadTest(unittest.TestCase):
 
-    @unittest.skipIf("TRAVIS" not in os.environ or os.environ["TRAVIS"] == "false",
-                     "This runs only on Travis CI")
+    # these tsts only run of a username and pw are in the environment variables
+    # can be set manually with export USERNAME="my_username" etc.
+    @unittest.skipIf("SMAPUSERNAME" not in os.environ or "SMAPPWD" not in os.environ, 'Username and/or PW not found')
     def test_full_download(self):
 
         dl_path = tempfile.mkdtemp()
-        startdate = "2018-12-01"
-        enddate = "2018-12-01"
+        startdate = enddate = "2018-12-01"
 
-        args = [dl_path, '-s', startdate, '-e', enddate, '--product', 'SPL3SMP.005', '--username', os.environ['USERNAME'], '--password',  os.environ['PWD']]
+        args = [dl_path, '-s', startdate, '-e', enddate, '--product', 'SPL3SMP.005',
+                '--username', os.environ['SMAPUSERNAME'], '--password',  os.environ['SMAPPWD']]
 
         main(args)
         time.sleep(50) #wait until download is finished, better solution?
-        #assert(os.listdir(dl_path) == ['2018.12.01', '2018.12.02'])
-        #assert(os.listdir(os.path.join(dl_path, '2018.12.01')) == ['SMAP_L3_SM_P_20181201_R16020_002.h5'])
-        #assert(os.listdir(os.path.join(dl_path, '2018.12.02')) == ['SMAP_L3_SM_P_20181202_R16020_001.h5'])
-        print(os.listdir(dl_path))
-        print(os.listdir(os.path.join(dl_path, '2018.12.01')))
+        assert(os.listdir(dl_path) == ['2018.12.01'])
+        assert(os.listdir(os.path.join(dl_path, '2018.12.01')) == ['SMAP_L3_SM_P_20181201_R16020_002.h5'])
