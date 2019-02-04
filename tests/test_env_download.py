ee45c4dd9e704c51217342b2f37a7f0ee2ef8c97
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2019-02-04 19:02:13 +0100

    Add download script test

--- tests/test_env_download.py
+++ tests/test_env_download.py
@@ -5,6 +5,26 @@ Tests that use passed login information from travis CI to download a few files
 '''
 
 import os
+import glob
+import tempfile
 
-def test_environment_var():
-    assert os.environ["TESTVAR"] == 'test'
+from smap_io.download import main
+import unittest
+import time
+
+class DownloadTest(unittest.TestCase):
+
+    @unittest.skipIf("TRAVIS" not in os.environ or os.environ["TRAVIS"] == "false", "This runs only on Travis CI")
+    def test_full_download(self):
+
+        dl_path = tempfile.mkdtemp()
+        startdate = "2018-12-01"
+        enddate = "2018-12-02"
+
+        args = [dl_path, '-s', startdate, '-e', enddate, '--product', 'SPL3SMP.005', '--username', os.environ['USERNAME'], '--password',  os.environ['PWD']]
+
+        main(args)
+        time.sleep(120) #wait until download is finished, better solution?
+        assert(os.listdir(dl_path) == ['2018.12.01', '2018.12.02'])
+        assert(os.listdir(os.path.join(dl_path, '2018.12.01')) == ['SMAP_L3_SM_P_20181201_R16020_002.h5'])
+        assert(os.listdir(os.path.join(dl_path, '2018.12.02')) == ['SMAP_L3_SM_P_20181202_R16020_001.h5'])
